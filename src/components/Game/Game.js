f56a7de0a9bebc23d568efbcfc3dbbea4109f755
Natalie Wee <natalieweesh@gmail.com>
2020-09-19 14:25:29 -0400

    fix the green border

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -39,7 +39,7 @@ const Game = ({ location }) => {
     let tileId = tile.element.id;
     // console.log('SET TILE CLASS', tileId, x, y, 'orderindex', user?.orderIndex);
 
-    if (x >= 525 && x < 1225 && y >= 443 && y < 1143 && user) {
+    if (x >= 490 && x < 1225 && y >= 443 && y < 1143 && user) {
       console.log("in no mans land", x, y, user)
       if (rotate && rotate > 0) {
         className = 'item rotate-' + rotate;
